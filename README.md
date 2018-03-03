0c4a004253cc2609c3a7e65772198825d6a6ae67
Jacob Mischka <jacob@mischka.me>
2018-03-03 00:31:28 -0600

    Add include and exclude options

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 Adds [`svg-react-loader`][loader] to gatsby webpack config (and removes `svg`s from the built-in `url-loader` config).
 
+`include` and `exclude` plugin options can be specified, if either is present then `svg-react-loader` will use them and `url-loader` will be reenabled with the inverse.
+
+The following uses `svg-react-loader` to process svgs from a path matching `/assets/`, and `url-loader` to process svgs from everywhere else.
+
+```js
+{
+    resolve: 'gatsby-plugin-react-svg',
+    options: {
+        include: /assets/
+    }
+}
+```
+
 [loader]: https://github.com/jhamlet/svg-react-loader
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,41 @@
-exports.modifyWebpackConfig = ({config, stage}) => {
+exports.modifyWebpackConfig = ({config, stage}, pluginOptions) => {
 	if ([
 		'develop',
 		'develop-html',
 		'build-html',
 		'build-javascript'
 	].includes(stage)) {
+		const { include, exclude } = pluginOptions;
+
 		// Remove svg from url-loader config
-		config.loader(`url-loader`, {
+		config.loader('url-loader', {
 			test: /\.(jpg|jpeg|png|gif|mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
-			loader: `url`,
+			loader: 'url-loader',
 			query: {
 				limit: 10000,
 				name: `static/[name].[hash:8].[ext]`,
-			},
-		})
+			}
+		});
+		
+		// Readd url-loader if in/excludes are specified
+		if (include || exclude) {
+			config.loader('url-loader-svg', {
+				test: /\.svg$/,
+				loader: 'url-loader',
+				query: {
+					limit: 10000,
+					name: `static/[name].[hash:8].[ext]`,
+				},
+				include: exclude,
+				exclude: include
+			});			
+		}
+		
 		config.loader('svg-react-loader', {
 			test: /\.svg$/,
-			loader: 'svg-react-loader'
+			loader: 'svg-react-loader',
+			include,
+			exclude
 		});
 	}
 
