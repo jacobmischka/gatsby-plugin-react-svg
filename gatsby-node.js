07d3151890296c19739a00ab77b7b177126a260b
Aaron Lampros <alampros@gmail.com>
2018-08-13 14:06:49 -0400

    Reenable include/exclude inversion

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,40 +1,50 @@
 exports.onCreateWebpackConfig = ({
-		stage, actions, getConfig, rules
+	stage, actions, getConfig, rules
 }, { rule: ruleProps = {} }) => {
-		if([
-				'develop',
-				'develop-html',
-				'build-html',
-				'build-javascript'
-		].includes(stage)) {
-				// Add the svg-react-loader rule
-				actions.setWebpackConfig({
-						module: {
-								rules: [
-										{
-												use: 'svg-react-loader',
-												test: /\.svg$/,
-												...ruleProps,
-										}
-								],
-						}
-				})
-				const cfg = getConfig()
-				const imgsRule = rules.images()
-				cfg.module.rules = [
-						// Remove the base url-loader images rule entirely
-						...cfg.module.rules.filter(rule => {
-								if(rule.test) {
-										return rule.test.toString() !== imgsRule.test.toString()
-								}
-								return true
-						}),
-						// Put it back without SVG loading
-						{
-								use: imgsRule.use,
-								test: new RegExp(imgsRule.test.toString().replace('svg|', '').slice(1, -1))
-						}
-				]
-				actions.replaceWebpackConfig(cfg)
+	const { include, exclude } = ruleProps
+
+	if([
+		'develop',
+		'develop-html',
+		'build-html',
+		'build-javascript'
+	].includes(stage)) {
+		// Add the svg-react-loader rule
+		actions.setWebpackConfig({
+			module: {
+				rules: [
+					{
+						use: 'svg-react-loader',
+						test: /\.svg$/,
+						...ruleProps,
+					}
+				],
+			}
+		})
+		const cfg = getConfig()
+		const imgsRule = rules.images()
+
+		const newUrlLoaderRule = (include || exclude) ? {
+			...imgsRule,
+			include: exclude,
+			exclude: include
+		} : {
+			...imgsRule,
+			test: new RegExp(imgsRule.test.toString().replace('svg|', '').slice(1, -1))
 		}
+
+		cfg.module.rules = [
+			// Remove the base url-loader images rule entirely
+			...cfg.module.rules.filter(rule => {
+				if(rule.test) {
+					return rule.test.toString() !== imgsRule.test.toString()
+				}
+				return true
+			}),
+			// Put it back without SVG loading
+			newUrlLoaderRule
+		]
+		actions.replaceWebpackConfig(cfg)
+	}
 }
+
