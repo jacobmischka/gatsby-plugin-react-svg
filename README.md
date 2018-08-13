e4ad07d1c4e76a5aa42438cc8738e6490f0755c4
Aaron Lampros <alampros@gmail.com>
2018-08-13 13:45:05 -0400

    Use gatsby@next APIs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Adds [`svg-react-loader`][loader] to gatsby webpack config (and removes `svg`s from the built-in `url-loader` config).
 
-`include` and `exclude` plugin options can be specified, if either is present then `svg-react-loader` will use them and `url-loader` will be reenabled with the inverse.
+The `rule` plugin option can be specified to pass [rule options](https://webpack.js.org/configuration/module/#rule).
 
 The following uses `svg-react-loader` to process svgs from a path matching `/assets/`, and `url-loader` to process svgs from everywhere else.
 
@@ -8,7 +8,9 @@ The following uses `svg-react-loader` to process svgs from a path matching `/ass
 {
     resolve: 'gatsby-plugin-react-svg',
     options: {
-        include: /assets/
+        rule: {
+          include: /assets/
+        }
     }
 }
 ```
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,40 @@
-exports.modifyWebpackConfig = ({config, stage}, pluginOptions) => {
-	if ([
-		'develop',
-		'develop-html',
-		'build-html',
-		'build-javascript'
-	].includes(stage)) {
-		const { include, exclude } = pluginOptions;
-
-		// Remove svg from url-loader config
-		config.loader('url-loader', {
-			test: /\.(jpg|jpeg|png|gif|mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
-			loader: 'url-loader',
-			query: {
-				limit: 10000,
-				name: `static/[name].[hash:8].[ext]`,
-			}
-		});
-		
-		// Readd url-loader if in/excludes are specified
-		if (include || exclude) {
-			config.loader('url-loader-svg', {
-				test: /\.svg$/,
-				loader: 'url-loader',
-				query: {
-					limit: 10000,
-					name: `static/[name].[hash:8].[ext]`,
-				},
-				include: exclude,
-				exclude: include
-			});			
+exports.onCreateWebpackConfig = ({
+		stage, actions, getConfig, rules
+}, { rule: ruleProps = {} }) => {
+		if([
+				'develop',
+				'develop-html',
+				'build-html',
+				'build-javascript'
+		].includes(stage)) {
+				// Add the svg-react-loader rule
+				actions.setWebpackConfig({
+						module: {
+								rules: [
+										{
+												use: 'svg-react-loader',
+												test: /\.svg$/,
+												...ruleProps,
+										}
+								],
+						}
+				})
+				const cfg = getConfig()
+				const imgsRule = rules.images()
+				cfg.module.rules = [
+						// Remove the base url-loader images rule entirely
+						...cfg.module.rules.filter(rule => {
+								if(rule.test) {
+										return rule.test.toString() !== imgsRule.test.toString()
+								}
+								return true
+						}),
+						// Put it back without SVG loading
+						{
+								use: imgsRule.use,
+								test: new RegExp(imgsRule.test.toString().replace('svg|', '').slice(1, -1))
+						}
+				]
+				actions.replaceWebpackConfig(cfg)
 		}
-		
-		config.loader('svg-react-loader', {
-			test: /\.svg$/,
-			loader: 'svg-react-loader',
-			include,
-			exclude
-		});
-	}
-
-	return config;
 }
